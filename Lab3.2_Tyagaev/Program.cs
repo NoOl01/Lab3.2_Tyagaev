d1b9baf24a1640f81cafa8c05b72c3895b7b5fcd
Anton <Anton@DESKTOP-6RE5PBQ>
2023-10-31 05:18:52 +0200

    Добавьте файлы проекта.

---
+++ Lab3.2_Tyagaev/Program.cs
@@ -0,0 +1,11 @@
+﻿//Lab 3.2. Номер: 12. Уровень: Высокий.
+int n = 0;
+do
+{
+    Console.Write("Введите число: ");
+    n = int.Parse(Console.ReadLine());
+    if ((n >= -5) && (n <= 5) && (n != 0)) Console.WriteLine($"число {n} лежит в итервале от -5 до 5");
+    else if (n == 0) Console.WriteLine("Цикл окончен");
+    else Console.WriteLine($"число {n} не лежит в итервале от -5 до 5");
+}
+while (n != 0);
